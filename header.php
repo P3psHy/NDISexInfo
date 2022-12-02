df8d068b6ae38585c6f749bc5b1b89f2e8e7e408
ApDnoze <100912264+ApDnoze@users.noreply.github.com>
2022-12-02 05:46:20 +0100

    t

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>SexInfo</title>
     <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="fond.css">
+    <link rel="stylesheet" href="informations.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
--- informations.php
+++ informations.php
@@ -15,40 +15,41 @@
         include "navbar.php" ; 
         include "fond.php";
     ?>
+    <div class="information-div">
+        <div class="titre">
+            <h3>Informations</h3>
+        </div>
 
-    <div class="titre">
-        <h3>Informations</h3>
-    </div>
-
-    <div class="central_div">
-        <div class="info_site">
-            <h4>Informations à propos de notre site</h4>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur jjjadipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur jjjadipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
+        <div class="central_div">
+            <div class="info_site">
+                <h4>Informations à propos de notre site</h4>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur jjjadipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur jjjadipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
+            </div>
+            <div class="lien">
+            <h4>Liens utiles :</h4>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur kkkadipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
+            </div>
+            <div class="savoir_plus">
+                <h4>Pour en savoir plus</h4>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur nnadipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
+            </div>
+            
         </div>
-        <div class="lien">
-        <h4>Liens utiles :</h4>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur kkkadipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
-        </div>
-        <div class="savoir_plus">
-            <h4>Pour en savoir plus</h4>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur nnadipisicing elit. Aliquam, recusandae, quaerat molestiae exercitationem ut architecto laborum labore rem neque quo inventore, in quod nesciunt soluta optio nostrum necessitatibus sint harum.</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, error velit nam dicta deserunt omnis. Mollitia, obcaecati ipsa eveniet alias facere omnis ipsum, sit iusto nulla quisquam quaerat, reiciendis perspiciatis!</p>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quisquam debitis laborum tempora facere autem dolor blanditiis, dicta vitae sit eaque voluptatem temporibus enim, ullam impedit beatae! Id, molestiae at.</p>
-        </div>
-        
     </div>
           
 </body>     
